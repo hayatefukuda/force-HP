ff51fc135a35051d3541655bc94466f9ec1f1408
yutowww <wydjent@gmail.com>
2021-01-15 10:06:17 +0900

    master pull

---
+++ force_hp/src/pages/access/access.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Mapping from "./map"
+
+import "./access.scss"
+
+const Access = () => {
+    return (
+        <Mapping />
+    )
+}
+
+export default Access;
--- force_hp/src/pages/access/map.js
+++ force_hp/src/pages/access/map.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
-export default class Maping extends Component {
+import "./access.scss"
+
+export default class Mapping extends Component {
   render() {
     const position = [35.6930823, 139.760481];
     return (
-      <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{ height: '100px' }}>
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
+      <div className="leaflet-container">
+        <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{ height: '100px' }}>
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={position}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
     )
   }
 }
