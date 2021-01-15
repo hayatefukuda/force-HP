02c165ab5a95b989c5b00986fffaab1aa2427553
YOSHIDARIE <rie.yoshida@force-corp.co.jp>
2021-01-15 09:47:50 +0900

    add image

--- force_hp/src/components/header.js
+++ force_hp/src/components/header.js
@@ -2,6 +2,7 @@ import React from "react"
 import Scrollspy from "react-scrollspy"
 import { Navbar, Nav } from "react-bootstrap"
 import Scroller from './scroller'
+import logo from '../images/force/logo-black.png'
 
 export default class Header extends React.Component {
   constructor(props) {
@@ -15,7 +16,7 @@ export default class Header extends React.Component {
         <Navbar className="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav" expand="lg"
                 collapseOnSelect={true}>
           <div className="container">
-            <a className="navbar-brand js-scroll-trigger" href="#page-top" onClick={Scroller.handleAnchorScroll}>Start Bootstrap</a>
+
             <Navbar.Toggle aria-controls="navbarResponsive"/>
             <Navbar.Collapse id="navbarResponsive">
               <Nav className="navbar-nav ml-auto my-2 my-lg-0">
@@ -23,16 +24,17 @@ export default class Header extends React.Component {
                            items={["about", "services", "portfolio", "contact"]}
                            currentClassName="active" rootEl={"#mainNav"} offset={-75}>
                   <li className="nav-item">
-                    <Nav.Link className={"js-scroll-trigger"} href="#about" onClick={Scroller.handleAnchorScroll}>About</Nav.Link>
+                    <Nav.Link className={"js-scroll-trigger"} href="#about" onClick={Scroller.handleAnchorScroll}>企業情報</Nav.Link>
                   </li>
                   <li className="nav-item">
-                    <Nav.Link className={"js-scroll-trigger"} href="#services" onClick={Scroller.handleAnchorScroll}>Services</Nav.Link>
+                    <Nav.Link className={"js-scroll-trigger"} href="#services" onClick={Scroller.handleAnchorScroll}>採用情報</Nav.Link>
+                  </li>
+                  <a className="navbar-brand js-scroll-trigger" href="#page-top" onClick={Scroller.handleAnchorScroll}><img src={logo} height={100}/></a>
+                  <li className="nav-item">
+                    <Nav.Link className={"js-scroll-trigger"} href="#portfolio" onClick={Scroller.handleAnchorScroll}>アクセス</Nav.Link>
                   </li>
                   <li className="nav-item">
-                    <Nav.Link className={"js-scroll-trigger"} href="#portfolio" onClick={Scroller.handleAnchorScroll}>Portfolio</Nav.Link>
-                  </li>
-                  <li className="nav-item">
-                    <Nav.Link className={"js-scroll-trigger"} href="#contact" onClick={Scroller.handleAnchorScroll}>Contact</Nav.Link>
+                    <Nav.Link className={"js-scroll-trigger"} href="#contact" onClick={Scroller.handleAnchorScroll}>お問い合わせ</Nav.Link>
                   </li>
                 </Scrollspy>
               </Nav>
@@ -41,16 +43,17 @@ export default class Header extends React.Component {
         </Navbar>
         <header className="masthead">
           <div className="container h-100">
-            <div className="row h-100 align-items-center justify-content-center text-center">
+            <div className="row h-100 align-items-center justify-content-center">
               <div className="col-lg-10 align-self-end">
-                <h1 className="text-uppercase text-white font-weight-bold">Your Favorite Source of Free Bootstrap
-                  Themes</h1>
-                <hr className="divider my-4"/>
+                <h1 className="text-uppercase text-white font-weight-bold">ITサービスで<br />道を拓く。</h1>
+                {/* <hr className="divider my-4"/> */}
               </div>
               <div className="col-lg-8 align-self-baseline">
-                <p className="text-white-75 font-weight-light mb-5">Start Bootstrap can help you build better websites
-                  using the Bootstrap framework! Just download a theme and start customizing, no strings attached!</p>
-                <a className="btn btn-primary btn-xl js-scroll-trigger" href="#about" onClick={Scroller.handleAnchorScroll}>Find Out More</a>
+                {/* <p className="text-white-75 font-weight-light mb-5">Start Bootstrap can help you build better websites
+                  using the Bootstrap framework! Just download a theme and start customizing, no strings attached!</p> */}
+                <div className="row h-100 align-items-center justify-content-center text-center">
+                <a className="btn btn-primary btn-xl js-scroll-trigger" href="#about" onClick={Scroller.handleAnchorScroll}>こちらから採用エントリー</a>
+                </div>
               </div>
             </div>
           </div>
