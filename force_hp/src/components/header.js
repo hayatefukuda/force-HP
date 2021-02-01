0d75dfe6e8b1c9575e46a1387af9620811e61699
yutowww <wydjent@gmail.com>
2021-02-01 15:48:14 +0900

    page transition footer

--- force_hp/src/components/header.js
+++ force_hp/src/components/header.js
@@ -14,40 +14,12 @@ export default class Header extends React.Component {
   render() {
     return (
       
-        // <Navbar className="navbar navbar-expand-lg navbar-light fixed-top py-0" id="mainNav" expand="lg"
-        //         collapseOnSelect={true}>
-        //   <div className="container">
-        //   <a className="navbar-brand js-scroll-trigger" href="#page-top" onClick={Scroller.handleAnchorScroll}><img src={logo} alt="フォース株式会社" height={100}/></a>
-        //     <Navbar.Toggle aria-controls="navbarResponsive"/>
-        //     <Navbar.Collapse id="navbarResponsive">
-        //       <Nav className="navbar-nav text-center">
-        //         <Scrollspy className="navbar-nav"
-        //                    items={["about", "services", "portfolio", "contact"]}
-        //                    currentClassName="active" rootEl={"#mainNav"} offset={-75}>                  
-        //           <li className="nav-item">
-        //             <Nav.Link className={"js-scroll-trigger"} href="#about"><Link to="/company_information/CompanyProfile">企業情報</Link></Nav.Link>
-        //           </li>
-        //           <li className="nav-item">
-        //             <Nav.Link className={"js-scroll-trigger"} href="#services"><Link to="/recruitment/recruit">採用情報</Link></Nav.Link>
-        //           </li>
-        //           <li className="nav-item">
-        //             <Nav.Link className={"js-scroll-trigger"} href="#portfolio"><Link to="/access/access">アクセス</Link></Nav.Link>
-        //           </li>
-        //           <li className="nav-item">
-        //             <Nav.Link className={"js-scroll-trigger"} href="#contact"><Link to="/company_information/CompanyProfile">お問い合わせ</Link></Nav.Link>
-        //           </li>
-        //         </Scrollspy>
-        //       </Nav>
-        //     </Navbar.Collapse>
-        //   </div>
-        // </Navbar>
-
-
-
         <Navbar className="navbar navbar-expand-lg navbar-light fixed-top py-0" id="mainNav" expand="lg"
                 collapseOnSelect={true}>
           <div className="container">
-          <a className="navbar-brand js-scroll-trigger" href="#page-top"><Link to="/"><img src={logo} alt="フォース株式会社" height={100}/></Link></a>
+          <a className="navbar-brand js-scroll-trigger" 
+            // href="#page-top"
+            ><Link to="/"><img src={logo} alt="フォース株式会社" height={100}/></Link></a>
             <Navbar.Toggle aria-controls="navbarResponsive"/>
             <Navbar.Collapse id="navbarResponsive">
               <Nav className="navbar-nav text-center">
@@ -55,27 +27,65 @@ export default class Header extends React.Component {
                            items={["about", "services", "portfolio", "contact"]}
                            currentClassName="active" rootEl={"#mainNav"} offset={-75}>                  
                   <li className="nav-item">
-                    
-                    <Link className={"js-scroll-trigger"} href="#services"
-                   to="/company_information/CompanyProfile">企業情報</Link>
+                    <Nav.Link className={"js-scroll-trigger"} 
+                    // href="#about"
+                    ><Link class="nav-link" to="/company_information/CompanyProfile">企業情報</Link></Nav.Link>
                   </li>
                   <li className="nav-item">
-                  <Link className={"js-scroll-trigger"} href="#services"
-                   to="/recruitment/recruit">採用情報</Link>
+                    <Nav.Link className={"js-scroll-trigger"} 
+                    // href="#services"
+                    ><Link class="nav-link" to="/recruitment/recruit">採用情報</Link></Nav.Link>
                   </li>
                   <li className="nav-item">
-                  <Link className={"js-scroll-trigger"} href="#services"
-                   to="/access/access">アクセス</Link>
+                    <Nav.Link className={"js-scroll-trigger"} 
+                    // href="#portfolio"
+                    ><Link class="nav-link" to="/access/access">アクセス</Link></Nav.Link>
                   </li>
                   <li className="nav-item">
-                  <Link className={"js-scroll-trigger"} href="#services"
-                   to="/company_information/CompanyProfile">お問い合わせ</Link>
+                    <Nav.Link className={"js-scroll-trigger"} 
+                    // href="#contact"
+                    ><Link class="nav-link" to="/company_information/CompanyProfile">お問い合わせ</Link></Nav.Link>
                   </li>
                 </Scrollspy>
               </Nav>
             </Navbar.Collapse>
           </div>
         </Navbar>
+
+
+
+        // <Navbar className="navbar navbar-expand-lg navbar-light fixed-top py-0" id="mainNav" expand="lg"
+        //         collapseOnSelect={true}>
+        //   <div className="container">
+        //   <a className="navbar-brand js-scroll-trigger" href="#page-top"><Link to="/"><img src={logo} alt="フォース株式会社" height={100}/></Link></a>
+        //     <Navbar.Toggle aria-controls="navbarResponsive"/>
+        //     <Navbar.Collapse id="navbarResponsive">
+        //       <Nav className="navbar-nav text-center">
+        //         <Scrollspy className="navbar-nav"
+        //                    items={["about", "services", "portfolio", "contact"]}
+        //                    currentClassName="active" rootEl={"#mainNav"} offset={-75}>                  
+        //           <li className="nav-item">
+                    
+        //             <Link className={"js-scroll-trigger"} href="#services"
+        //            to="/company_information/CompanyProfile">企業情報</Link>
+        //           </li>
+        //           <li className="nav-item">
+        //           <Link className={"js-scroll-trigger"} href="#services"
+        //            to="/recruitment/recruit">採用情報</Link>
+        //           </li>
+        //           <li className="nav-item">
+        //           <Link className={"js-scroll-trigger"} href="#services"
+        //            to="/access/access">アクセス</Link>
+        //           </li>
+        //           <li className="nav-item">
+        //           <Link className={"js-scroll-trigger"} href="#services"
+        //            to="/company_information/CompanyProfile">お問い合わせ</Link>
+        //           </li>
+        //         </Scrollspy>
+        //       </Nav>
+        //     </Navbar.Collapse>
+        //   </div>
+        // </Navbar>
 
     );
   }
--- force_hp/src/pages/company_information/Home.jsx
+++ force_hp/src/pages/company_information/Home.jsx
@@ -1,4 +1,6 @@
+import { Link } from "gatsby"
 import React from "react"
+import Layout from "../../components/layout"
 import Company_home from "../../images/force/company-home.png"
 import Greeting from "../../images/force/icon-greeting.png"
 
@@ -7,29 +9,31 @@ import "./Home.scss"
 const Home = () => {
 
     return (
-        <div>
-            <div className="heading">
-                <img src={ Company_home } className="img-fluid" alt=""/>
-                <p>企業情報</p>
-            </div>
-            <div className="container">
-                <p　className="mt-5 font-weight-bold">企業情報を表示します。</p>
-            </div>
-            <div className="d-flex justify-content-center button">
-                <div className="greetings">
-                    <a href="#" className="text-reset">
-                        ごあいさつ
-                        <img src={ Greeting } />
-                    </a>
+        <Layout>
+            <div>
+                <div className="heading">
+                    <img src={ Company_home } className="img-fluid" alt=""/>
+                    <p>企業情報</p>
                 </div>
-                <div className="business_content">
-                    <a href="#" className="ml-5 text-reset">事業内容</a>
+                <div className="container">
+                    <p className="mt-5 font-weight-bold">企業情報を表示します。</p>
                 </div>
-                <div className="company_profile">
-                    <a href="#" className="ml-5 text-reset">会社概要</a>
+                <div className="d-flex justify-content-center button">
+                    <div className="greetings">
+                        <Link to="/company_information/Greeting" className="text-reset">
+                            ごあいさつ
+                            <img src={ Greeting } />
+                        </Link>
+                    </div>
+                    <div className="business_content">
+                        <Link to="/company_information/BusinessContent" className="ml-5 text-reset">事業内容</Link>
+                    </div>
+                    <div className="company_profile">
+                        <Link to="/company_information/CompanyProfile" className="ml-5 text-reset">会社概要</Link>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Layout>
     )
 }
 
